42564e89fadc3ebb133a513e5bea507551d3b807
Saxon-James <saxondenny@live.co.uk>
2019-02-04 17:32:51 +0000

    update

---
+++ javascript/scroll-reveal.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+    var one = document.getElementById('ProOne');
+    window.sr = new ScrollReveal()
+    sr.reveal(one, {delay: 200, reset: true, desktop: true, duration: 2500, origin: 'top', distance: '100px', });
+    });
+$(document).ready(function() {
+    var two = document.getElementById('ProTwo');
+    window.sr = new ScrollReveal()
+    sr.reveal(two, {delay: 200, reset: true, desktop: true, duration: 2500, origin: 'bottom', distance: '100px', });
+});
+$(document).ready(function() {
+    var four = document.getElementById('ProFour');
+    window.sr = new ScrollReveal()
+    sr.reveal(four, {delay: 200, reset: true, desktop: true, duration: 2500, origin: 'top', distance: '100px', });
+});
+$(document).ready(function() {
+    var five = document.getElementById('ProFive');
+    window.sr = new ScrollReveal()
+    sr.reveal(five, {delay: 200, reset: true, desktop: true, duration: 2500, origin: 'bottom', distance: '100px', });
+});
+
+
+
